3a3188994980e6040600991d5edc833614e96e35
ricsonl <ricsonluizv@gmail.com>
2020-07-29 01:58:32 -0300

    minor changes and bug fix

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,7 +6,9 @@ import logo from '../../assets/visual_test.svg';
 const Logo = (props:any) => {
     return (
         <div className="logo">
-            <svg className="more" onClick={() => props.toggle(true)}><use xlinkHref={`#${props.icon}`}></use></svg>
+            <div onClick={() => props.toggle(true)}>
+                <svg className="moreicon"><use xlinkHref={`#${props.icon}`}></use></svg>
+            </div>
             <img height={35} src={logo} alt="g" />
         </div>
     )
