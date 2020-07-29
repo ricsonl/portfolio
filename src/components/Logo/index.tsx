57a3ddc8f42012e10d0bddc200d7620378d2f967
ricsonl <ricsonluizv@gmail.com>
2020-07-29 12:08:45 -0300

    (optimization) removed logo import and reduced css specificity

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './styles.css';
 
-import logo from '../../assets/visual_test.svg';
-
 const Logo = (props:any) => {
     return (
-        <div className="logo">
+        <div className="area">
             <div onClick={() => props.toggle(true)}>
                 <svg className="moreicon"><use xlinkHref={`#${props.icon}`}></use></svg>
             </div>
-            <img height={35} src={logo} alt="g" />
+            <svg className="logo"><use xlinkHref='#logo'></use></svg>
         </div>
     )
 }
