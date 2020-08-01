310807af90a6143cad28403845dd82d7bda2eae4
ricsonl <ricsonluizv@gmail.com>
2020-08-01 00:17:04 -0300

    home effects and some fixes

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './styles.css';
 
 const Contact = (props: any) => {
+
     return (
         <section>
             <div className="contact" ref={props.contentRef}>
-                <h1>Contact</h1>
-                <p>bla bla bla</p>
+                <div className="inner">
+                    <h1>Contact</h1>
+                    <p>bla bla bla</p>
+                </div>
             </div>
         </section>
     )
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import './styles.css';
 
 const Home = (props:any) => {
-    
+
     const parallax = () => {
         const offset = window.pageYOffset;
-        /*const el = document.querySelector(".home-content");
-        if(el)
-            (el as HTMLElement).style.backgroundPositionY = offset * 0.5 + "px";
-        */
         if(props.contentRef.current)
             props.contentRef.current.style.backgroundPositionY = offset * 0.5 + "px";
+        if(offset < window.innerHeight){
+            const spacing = document.getElementsByClassName("spacing");
+            for (let i = 0; i < spacing.length; i++) {
+                (spacing[i] as HTMLElement).style.letterSpacing = 3 + offset * .05 + "px";
+            }
+        }
     };
 
     window.addEventListener("scroll", parallax);
     
     return (
         <section>
+            <div className="skew"></div>
             <div className="home-content" ref={props.contentRef}>
                 <div className="inner">
-                    <h1>Home</h1>
-                    <p> 
-                        bla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla
-                        blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla
-                        bla blabla bla blabla bla blabla bla blabla
-                    </p>
+                    <h1 className="spacing">Ricson Vilaça</h1>
+                    <p className="home-tag spacing">&nbsp;&lt;/&gt;</p>
+                    <p className="home-sub spacing">software developer</p>
                 </div>
             </div>
         </section>
